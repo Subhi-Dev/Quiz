4de63c2c067ec18b921c0231b6741ed62b67e299
Subhi-Dev <53143122+Subhi-Dev@users.noreply.github.com>
2019-07-24 21:52:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,177 @@
+const electron = require('electron')
+const path = require('path')
+const BrowserWindow = electron.remote.BrowserWindow
+const app = electron.app
+const url = require('url')
+const Menu = electron.Menu
+const fs = require('fs')
+
+function open1lev() {
+    const levpath = path.join('file://',__dirname,'1lev/1lev.html')
+    let levwin = new BrowserWindow({alwaysOnTop: true})
+    levwin.loadURL(levpath);
+    levwin.show()
+    levwin.on('closed', () => {
+        check()
+    })
+}
+function open2lev() {
+    const levpath = path.join('file://',__dirname,'2lev/2lev.html')
+    let levwin = new BrowserWindow({alwaysOnTop: true})
+    levwin.loadURL(levpath);
+    levwin.show()
+    levwin.on('closed', () => {
+      check()
+  })
+
+}
+function open3lev() {
+    const levpath = path.join('file://',__dirname,'3lev/3lev.html')
+    let levwin = new BrowserWindow({alwaysOnTop: true})
+    levwin.loadURL(levpath);
+    levwin.show()
+    levwin.on('closed', () => {
+      check()
+  })
+
+}
+function open4lev() {
+    const levpath = path.join('file://',__dirname,'4lev/4lev.html')
+    let levwin = new BrowserWindow({alwaysOnTop: true})
+    levwin.loadURL(levpath);
+    levwin.show()
+    levwin.on('closed', () => {
+      check()
+  })
+
+}
+function open5lev() {
+    const levpath = path.join('file://',__dirname,'5lev/5lev.html')
+    let levwin = new BrowserWindow({alwaysOnTop: true})
+    levwin.loadURL(levpath);
+    levwin.show()
+    levwin.on('closed', () => {
+      check()
+  })
+
+}
+function open6lev() {
+    const levpath = path.join('file://',__dirname,'6lev/6lev.html')
+    let levwin = new BrowserWindow({alwaysOnTop: true})
+    levwin.loadURL(levpath);
+    levwin.show()
+    levwin.on('closed', () => {
+      check()
+  })
+
+}
+function open7lev() {
+    const levpath = path.join('file://',__dirname,'7lev/7lev.html')
+    let levwin = new BrowserWindow({alwaysOnTop: true})
+    levwin.loadURL(levpath);
+    levwin.show()
+    levwin.on('closed', () => {
+      check()
+  })
+
+}
+function open8lev() {
+    const levpath = path.join('file://',__dirname,'8lev/8lev.html')
+    let levwin = new BrowserWindow({alwaysOnTop: true})
+    levwin.loadURL(levpath);
+    levwin.show()
+    levwin.on('closed', () => {
+      check()
+  })
+
+}
+function open9lev() {
+    const levpath = path.join('file://',__dirname,'9lev/9lev.html')
+    let levwin = new BrowserWindow({alwaysOnTop: true})
+    levwin.loadURL(levpath);
+    levwin.show()
+    levwin.on('closed', () => {
+      check()
+  })
+
+}
+function open10lev() {
+    const levpath = path.join('file://',__dirname,'1lev/1lev.html')
+    let levwin = new BrowserWindow({alwaysOnTop: true})
+    levwin.loadURL(levpath);
+    levwin.show()
+    levwin.on('closed', () => {
+      check()
+  })
+
+}
+check()
+function check() {
+  var v = "C:/Users/Code/Desktop/100-Great";
+
+    fs.readFile(v.concat('/1lev/1lev.txt'), 'utf-8', (err, data) => {
+      if (data == "3") {
+        document.getElementById("two").disabled = false;
+        data = "0";
+      }
+    });
+    fs.readFile(v.concat('/2lev/2lev.txt'), 'utf-8', (err, data) => {
+        
+      if (data == "3") {
+        document.getElementById("thr").disabled = false;
+        data = "0";
+
+      }
+    });
+    fs.readFile(v.concat('/3lev/3lev.txt'), 'utf-8', (err, data) => {
+        
+      if (data == "3") {
+        document.getElementById("fou").disabled = false;
+        data = "0";
+      }
+    });
+    fs.readFile(v.concat('/4lev/4lev.txt'), 'utf-8', (err, data) => {
+        
+      if (data == "3") {
+        document.getElementById("fiv").disabled = false;
+        data = "0";
+      }
+    });
+    fs.readFile(v.concat('/5lev/5lev.txt'), 'utf-8', (err, data) => {
+        
+      if (data == "3") {
+        document.getElementById("six").disabled = false;
+        data = "0";
+      }
+    });
+    fs.readFile(v.concat('/6lev/6lev.txt'), 'utf-8', (err, data) => {
+        
+      if (data == "3") {
+        document.getElementById("sev").disabled = false;
+        data = "0";
+      }
+    });
+    fs.readFile(v.concat('/7lev/7lev.txt'), 'utf-8', (err, data) => {
+        
+      if (data == "3") {
+        document.getElementById("eig").disabled = false;
+        data = "0";
+
+      }
+    });
+    fs.readFile(v.concat('/8lev/8lev.txt'), 'utf-8', (err, data) => {
+        
+      if (data == "3") {
+        document.getElementById("nin").disabled = false;
+        data = "0";
+
+      }
+    });
+    fs.readFile(v.concat('/9lev/9lev.txt'), 'utf-8', (err, data) => {
+        
+      if (data == "3") {
+        document.getElementById("ten").disabled = false;
+        data = "0";
+      }
+    });
+}
